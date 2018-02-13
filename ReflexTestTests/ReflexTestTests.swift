689b77c06554bc9ab8d24beeadb9e305edb0e108
Jon Lowe <cheesuschrystler@gmail.com>
2018-02-12 20:44:58 -0500

    unit tests for model

--- ReflexTestTests/ReflexTestTests.swift
+++ ReflexTestTests/ReflexTestTests.swift
@@ -9,28 +9,51 @@
 import XCTest
 @testable import ReflexTest
 
+var timeTrialTest: TimeTrial!
 class ReflexTestTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        timeTrialTest = TimeTrial()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        timeTrialTest = nil
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Double check to insure you cant invalidate a timer that doesnt exist.
+    func testNilTimerStopping() {
+        XCTAssertNil(timeTrialTest.timer, "Timer should be nil")
+        XCTAssertNoThrow(timeTrialTest.stopTimer(), "Timer stopping should not throw error")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    // Verify the various states of the timer.
+    func testTimerState() {
+        XCTAssertNil(timeTrialTest.timer, "Timer should begin as nil.")
+        XCTAssertTrue(timeTrialTest.beginTimer(), "Timer should return successfully.")
+        XCTAssert((timeTrialTest.timer?.isValid)!, "Timer should be valid after beginning.")
+        timeTrialTest.stopTimer()
+        XCTAssertNil(timeTrialTest.timer, "Timer should be nil again.")
+    }
+    
+    // Verify that only five trials can be completed.
+    func testMaximumNumberOfTrials() {
+        let expected = 5
+        for index in 1...7 {
+            if (index <= 5) {
+                XCTAssertTrue(timeTrialTest.beginTimer(), "Timer should return successfully for 5 tries.")
+                XCTAssertNotNil(timeTrialTest.timer, "Timer should exist.")
+                XCTAssertTrue((timeTrialTest.timer?.isValid)!, "Timer should be valid.")
+                timeTrialTest.stopTimer()
+            } else {
+                XCTAssertFalse(timeTrialTest.beginTimer(), "Timer should not return successfully after 5 trials.")
+                XCTAssertNil(timeTrialTest.timer, "Timer should not exist.")
+            }
         }
+        
+        XCTAssertEqual(timeTrialTest.countCompletedTrials(), expected, "There should be 5 trials logged.")
     }
-    
 }
